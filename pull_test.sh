eeed11f69ebc9c46ec9090c79451c5368ad7f08f
Siim Tiilen <eritikass@gmail.com>
2019-05-23 22:13:41 +0300

    test

---
+++ pull_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+main() {
+    local name="$1"
+    local img="805248565926.dkr.ecr.eu-west-1.amazonaws.com/foo/${name}:test"
+    if docker pull "$img";
+    then
+        echo "OK -> $img"
+    else
+        echo "ERROR -> $img"
+    fi
+}
+
+main "$@"
